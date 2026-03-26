c42f55e7f3d343760554043426cbc55e406c4208
maou-shonen <q267009886.tw@gmail.com>
2026-03-26 04:12:21 +0000

    feat: Tauri entry point with hook installation and engine thread

---
+++ src-tauri/src/state.rs
@@ -0,0 +1,13 @@
+use std::sync::{Arc, Mutex};
+
+use crossbeam_channel::Sender;
+
+use crate::config::ConfigStore;
+use crate::threshold::AppThresholdCache;
+use crate::traits::EngineCommand;
+
+pub struct AppState {
+    pub engine_tx: Sender<EngineCommand>,
+    pub config_store: Arc<dyn ConfigStore>,
+    pub threshold_cache: Arc<Mutex<AppThresholdCache>>,
+}
